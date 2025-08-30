9845bc9af622e1d4f78904991d65831b3727e3e0
Rxm×. <gautier.klein5@gmail.com>
2025-08-30 16:21:04 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Rxm'
-description 'Système d’ID unique pour ESX Legacy'
+description 'Id_unique'
 version '1.0.0'
 
 shared_script 'config.lua'
 server_script 'server.lua'
+
